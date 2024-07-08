d4933562b5682427f631d4d9aa39e0b67633afd4
Осинцев Павел <pavel.osincev04@mail.ru>
2024-07-08 22:47:24 +0500

    Создание в методе Update базовой обработки сообщений: фотосообщение; видеосообщений; аудиосообщений; стикеров; текстовых сообщений

--- InvestBot/Program.cs
+++ InvestBot/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace InvestBot
 {
@@ -7,7 +8,9 @@ namespace InvestBot
     {
         static void Main(string[] args)
         {
+            //отправляем в наш конструктор наш токен
             var client = new TelegramBotClient("5889212515:AAEcH4MhXVs7osVVlr7h5vgK4IacOEaKoAI");
+            //задаем методы обработки
             client.StartReceiving(Update, Error);
             Console.ReadLine();
         }
@@ -16,7 +19,43 @@ namespace InvestBot
 
         async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
         {
-            throw new NotImplementedException();
+
+            var message = update.Message;
+            
+            switch (message.Type)
+            {
+                case MessageType.Photo:
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat.Id, "Я не воспринимаю фотографии");
+                        return;
+                    }
+                case MessageType.Video: 
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat.Id, "Я не воспринимаю видео");
+                        return; 
+                    }
+                
+                case MessageType.Audio:
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat.Id, "Я не воспринимаю аудиосообщения");
+                        return;
+                    }
+
+                case MessageType.Sticker: 
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat.Id, "Я не воспринимаю стикеры");
+                        return;
+                    }
+            }
+
+            if (message.Text != null)
+            {
+                if (message.Text == "/start")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Привет");
+                    return;
+                }
+            }
         }
 
         private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
