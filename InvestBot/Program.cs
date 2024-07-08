11afd52b6e40000dda227bdebd6aa654a9f935a0
Осинцев Павел <pavel.osincev04@mail.ru>
2024-07-08 22:05:56 +0500

    Добавьте файлы проекта.

---
+++ InvestBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using Telegram.Bot;
+
+namespace InvestBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
