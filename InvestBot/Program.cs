4c5415708b5ac08cb0bf15cc7b190dd3a32f757d
Осинцев Павел <pavel.osincev04@mail.ru>
2024-07-09 17:31:34 +0500

    Обновление приветсвие и создание клавиатурных кнопок боту

--- InvestBot/Program.cs
+++ InvestBot/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using System.IO;
+using Telegram.Bot.Types.ReplyMarkups;
+using System.Collections.Generic;
 
 namespace InvestBot
 {
@@ -22,12 +25,19 @@ namespace InvestBot
 
             var message = update.Message;
             
+            //смотрим какой тип сообщения отправил пользователь, если тип не доступный для обработки - бот это уведомит
             switch (message.Type)
             {
                 case MessageType.Photo:
                     {
-                        await botClient.SendTextMessageAsync(message.Chat.Id, "Я не воспринимаю фотографии");
-                        return;
+                        
+
+                            //@"F:\Проекты\photo\aa.jpg"
+                   
+                            await botClient.SendTextMessageAsync(message.Chat.Id, "Я не воспринимаю фотографии");
+                            return;
+                        
+                       
                     }
                 case MessageType.Video: 
                     {
@@ -48,15 +58,32 @@ namespace InvestBot
                     }
             }
 
+            //если было отправленно текстовое сообщение, то бот начнет обработку тектсовых команд
             if (message.Text != null)
             {
+                //приветствие
                 if (message.Text == "/start")
                 {
-                    await botClient.SendTextMessageAsync(message.Chat.Id, "Привет");
+                    //создание клавиатурных кнопок
+                    var buttons = new KeyboardButton[][]
+                    {
+                        new KeyboardButton[] { "Главная страница","О пет-проекте" },
+                        new KeyboardButton[] { "Уроки инвестирвония", "Глоссарий инвестора" },
+                    };
+                    //Приветсвие 
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Привет, я бот РБО! Я начну тебя базовым навыкам инвестирования.\n" +
+                        "\nТы познакомишься с фондовым рынком и узнаешь как правильного подбирать компании для своего портфеля, что поможет тебе получить новый заработок.");
+                    //Вывод на экран клавиатуры
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Теперь начнем тебя обучать!",
+                       replyMarkup:  new ReplyKeyboardMarkup(buttons) { ResizeKeyboard = true });
                     return;
                 }
             }
         }
+
+
+
+        
 
         private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
         {
